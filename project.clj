2506e30201bf3bdc896cfb2804de603afbe60834
Gabriel Pedrosa <gabrielmip@protonmail.com>
2020-11-29 18:37:44 -0300

    Consertando versao no project.clj e permitindo geraçao do uberjar

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marklens "0.1.0-SNAPSHOT"
+(defproject marklens "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -16,7 +16,7 @@
                     [org.clojure/clojure org.clojure/data.xml]]
 
                  [clojure.java-time "0.3.2"]]
-  :main ^:skip-aot marklens.core
+  :main marklens.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
