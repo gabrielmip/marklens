93c79e3013c1c9f21fa7300075a2de327ba88482
Gabriel Pedrosa <gabrielmip@protonmail.com>
2020-12-19 18:09:40 -0300

    Refatoração da função de busca

--- src/marklens/search_engine/searcher.clj
+++ src/marklens/search_engine/searcher.clj
@@ -24,25 +24,36 @@
         (merge doc (get term-stats-by-id (:term_id doc))))
       doc-term-stats)))
 
-(defn get-doc-term-stats
+(defn get-doc-term-stats!
   [term-ids]
   (merge-doc-and-term-stats
     (storage/query-term-stats! term-ids)
     (storage/query-doc-term-stats! term-ids)))
 
+(defn tensorize-docs-and-query
+  [query-term-ids]
+  (let [doc-term-stats (get-doc-term-stats! query-term-ids)
+        term-stats (storage/query-term-stats! query-term-ids)]
+    {:docs (tensorizer/tensorize-documents doc-term-stats query-term-ids)
+     :query (tensorizer/tensorize-query query-term-ids term-stats)}))
+
+(defn assoc-rank-result-in-doc
+  [rank-result documents-by-id]
+  (assoc (get documents-by-id (:document_id rank-result))
+         :tensor (:tensor rank-result)
+         :similarity (:similarity rank-result)))
+
+(defn get-best-n-documents-from-tensors
+  [doc-tensors query-tensor n-results]
+  (let [results (take n-results (ranking/rank-documents doc-tensors query-tensor))
+        documents-by-id (storage/get-indexed-documents! (map #(:document_id %) results))]
+    (map
+      #(assoc-rank-result-in-doc % documents-by-id)
+      results)))
+
 (defn search
   ([query] (search query 10))
   ([query n-results]
-   (let [terms (extract-terms query)
-         term-ids (storage/query-term-ids! terms)
-         doc-term-stats (get-doc-term-stats term-ids)
-         term-stats (storage/query-term-stats! term-ids)
-         tensorized-docs (tensorizer/tensorize-documents doc-term-stats term-ids)
-         query-tensor (tensorizer/tensorize-query term-ids term-stats)
-         results (take n-results (ranking/rank-documents tensorized-docs query-tensor))
-         documents (storage/get-indexed-documents! (map #(:document_id %) results))]
-     (map
-      #(assoc (get documents (:document_id %))
-              :tensor (:tensor %)
-              :similarity (:similarity %))
-      results))))
+   (let [query-term-ids (storage/query-term-ids! (extract-terms query))
+         {doc-tensors :docs query-tensor :query} (tensorize-docs-and-query query-term-ids)]
+     (get-best-n-documents-from-tensors doc-tensors query-tensor n-results))))
