397d5a6c10a48b0260b79e05ac3b557dce34e0c3
Gabriel Pedrosa <gabriel@alude.com.br>
2021-07-04 20:50:52 -0300

    Formatação

--- src/marklens/utils.clj
+++ src/marklens/utils.clj
@@ -4,14 +4,14 @@
   "maps indexer-field to value-field"
   [value-field indexer-field values]
   (into {}
-    (map
-      (fn [item] [(indexer-field item) (value-field item)])
-      values)))
+        (map
+         (fn [item] [(indexer-field item) (value-field item)])
+         values)))
 
 (defn index-by
   [field items]
   (reduce
-    (fn [cur item]
-      (assoc cur (field item) item))
-    {}
-    items))
+   (fn [cur item]
+     (assoc cur (field item) item))
+   {}
+   items))
